6845362eec7233122415173b687d69f763d04f6c
deezlan <danial.eizlan@yahoo.com>
2024-06-25 23:35:14 +0800

    Danial test branch update

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,7 +7,7 @@ public class Main {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
-        window.setTitle("Slay the Empiresssssssssssssssssssss");
+        window.setTitle("Slay the Empiresssssssssssssssssssss danial update");
 
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);

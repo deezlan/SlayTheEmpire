608921195d8e073e507d2773d862c068edcc1c17
PeePeePooPoo <bhowjk@gmail.com>
2024-06-26 00:04:18 +0800

    Second test commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,7 +7,7 @@ public class Main {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
-        window.setTitle("Slay the Empiresssssssssssssssssssss");
+        window.setTitle("Slay the Empiresssssssssssssssssssss brandon update");
 
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);

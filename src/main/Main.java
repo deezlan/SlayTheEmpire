4d1fc03d18360b88bad5658f5b86fad93bd14462
deezlan <danial.eizlan@yahoo.com>
2024-06-26 14:09:22 +0800

    merge test by Danial

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,7 +7,7 @@ public class Main {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
-        window.setTitle("Slay the Empiresssssssssssssssssssss");
+        window.setTitle("Slay the Empiress test merge by Danial");
 
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);

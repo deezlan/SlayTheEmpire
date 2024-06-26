a4e5fcb1681bac35996e7d94e5bc795722b127b6
deezlan <danial.eizlan@yahoo.com>
2024-06-27 02:33:43 +0800

    spaced Entity class variables

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -12,50 +12,13 @@ public class Entity {
             idleLeft1, idleLeft2, idleLeft3, idleLeft4, idleLeft5, idleLeft6,
             moveLeft1, moveLeft2, moveLeft3, moveLeft4, moveLeft5, moveLeft6, moveLeft7, moveLeft8,
             moveRight1, moveRight2, moveRight3, moveRight4, moveRight5, moveRight6, moveRight7, moveRight8;
+
     public String action;
     public boolean lookingRight;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     public int spriteCounter = 0;
     public int spriteNum = 1;
+
     public Rectangle solidArea;
     public boolean collisionOn = false;
 }

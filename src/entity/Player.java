7bac5ecbfcf5b167a965dae8f09cb9b04d0dba9d
HedgieLovesCoding <danial.eizlan@yahoo.com>
2024-07-18 21:21:48 +0800

    fixed player not spawning on retry

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -31,7 +31,7 @@ public class Player extends Entity {
     private int delta;
 
     public Player (GamePanel gp, KeyHandler keyH, Cursor cursor, int playerClass) {
-        super(gp, 303, 9);
+        super(gp);
         this.gp = gp;
         this.keyH = keyH;
         this.playerClass = playerClass;
@@ -127,6 +127,8 @@ public class Player extends Entity {
         //add inventory
     }
     public void setDefaultPosition() {
+        worldX = 303;
+        worldY = 9;
         gp.currentMap = 0;
         action = "idleRight";
     }
@@ -297,9 +299,9 @@ public class Player extends Entity {
             currentLife = maxLife;
         }
 
-        if (attacking)
+        if (attacking) {
             startAttack();
-        else {
+        } else {
 
             if ((keyH.wPressed && keyH.sPressed) || (keyH.aPressed && keyH.dPressed)) {
                 action = "stuckOppositeDirection";

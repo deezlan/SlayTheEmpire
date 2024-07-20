23cfeb5c0bc2f4a2b78e63abee722a9f53cd448f
zakyh <zaky.hakim@gmail.com>
2024-07-21 00:30:49 +0800

    made gp.showcursor public

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -199,7 +199,7 @@ public class GamePanel extends JPanel implements Runnable {
                     (cursorImg, new Point(0,0), "blank cursor");
         this.setCursor(blankCursor);
     }
-    private void showCursor() {
+    public void showCursor() {
         java.awt.Cursor defaultCursor = java.awt.Cursor.getDefaultCursor();
         this.setCursor(defaultCursor);
     }
